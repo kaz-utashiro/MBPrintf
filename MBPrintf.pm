2cc535b92f431d510b2ecd63994c34db497f67d5
Kazumasa Utashiro <kaz@utashiro.com>
2011-07-24 16:56:51 +0900

    small change to avoid unexpected behavior.

--- MBPrintf.pm
+++ MBPrintf.pm
@@ -7,7 +7,7 @@ BEGIN {
     use Exporter   ();
     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 
-    $VERSION = sprintf "%d.%03d", q$Revision: 1.1 $ =~ /(\d+)/g;
+    $VERSION = sprintf "%d.%03d", q$Revision: 1.2 $ =~ /(\d+)/g;
 
     @ISA         = qw(Exporter);
     @EXPORT      = qw(&mbprintf &mbsprintf);
@@ -16,6 +16,8 @@ BEGIN {
 }
 our @EXPORT_OK;
 
+END { }
+
 my $wchar_re;
 if (1) {
     $wchar_re = qr/[\p{East_Asian_Width=Wide}\p{East_Asian_Width=FullWidth}]/;
@@ -23,11 +25,9 @@ if (1) {
     $wchar_re = qr/[\p{East_Asian_Width=Wide}\p{East_Asian_Width=FullWidth}\p{East_Asian_Width=Ambiguous}]/;
 }
 
-END { }
-
 sub mbsprintf {
     my($format, @args) = @_;
-    my %table;
+    my @list;
 
     my $n = 0;
     for my $arg (@args) {
@@ -36,16 +36,17 @@ sub mbsprintf {
 
 	my $len;
 	my $save = $arg;
-	while ($arg =~ m/($wchar_re)|./g) {
-	    $len += $1 ? 2 : 1;
+	while ($arg =~ m/($wchar_re+)|./g) {
+	    $len += $1 ? length($1) * 2 : 1;
 	}
-	$arg = pack("CC", $n/5+1, $n%5+1) . ("_" x ($len - 2));
-	$table{$arg} = $save;
+	$arg = pack("CC", $n / 5 + 1, $n % 5 + 1) . ("_" x ($len - 2));
+	push(@list, [$arg, $save]);
 	$n++;
     }
     my $result = sprintf($format, @args);
-    while (my($k, $v) = each %table) {
-	$result =~ s/$k/$v/;
+    while (@list) {
+	my($from, $to) = @{shift @list};
+	$result =~ s/$from/$to/;
     }
     $result;
 }
