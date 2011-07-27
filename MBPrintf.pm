2dabfb103f568c6c0e1e6ae1a314a0c5ae9976c7
Kazumasa Utashiro <kaz@utashiro.com>
2011-07-27 12:10:44 +0900

    New exportable func mbwidth(). Use closure func to produce unique strings.

--- MBPrintf.pm
+++ MBPrintf.pm
@@ -3,16 +3,18 @@ package MBPrintf;
 use strict;
 use warnings;
 
+use Carp;
+
 BEGIN {
     use Exporter   ();
     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 
-    $VERSION = sprintf "%d.%03d", q$Revision: 1.2 $ =~ /(\d+)/g;
+    $VERSION = sprintf "%d.%03d", q$Revision: 1.3 $ =~ /(\d+)/g;
 
     @ISA         = qw(Exporter);
     @EXPORT      = qw(&mbprintf &mbsprintf);
     %EXPORT_TAGS = ( );
-    @EXPORT_OK   = qw();
+    @EXPORT_OK   = qw(&mbwidth);
 }
 our @EXPORT_OK;
 
@@ -29,23 +31,17 @@ sub mbsprintf {
     my($format, @args) = @_;
     my @list;
 
-    my $n = 0;
+    my $uniqstr = _sub_uniqstr();
     for my $arg (@args) {
 	next if not defined $arg;
 	next if $arg !~ /$wchar_re/;
 
-	my $len;
 	my $save = $arg;
-	while ($arg =~ m/($wchar_re+)|./g) {
-	    $len += $1 ? length($1) * 2 : 1;
-	}
-	$arg = pack("CC", $n / 5 + 1, $n % 5 + 1) . ("_" x ($len - 2));
-	push(@list, [$arg, $save]);
-	$n++;
+	$arg = $uniqstr->(mbwidth($arg));
+	push(@list, $arg, $save);
     }
     my $result = sprintf($format, @args);
-    while (@list) {
-	my($from, $to) = @{shift @list};
+    while (my($from, $to) = splice(@list, 0, 2)) {
 	$result =~ s/$from/$to/;
     }
     $result;
@@ -53,6 +49,27 @@ sub mbsprintf {
 
 sub mbprintf {
     print mbsprintf(@_);
+}
+
+sub mbwidth {
+    my $arg = shift;
+    my $len;
+    while ($arg =~ m/($wchar_re+)|./g) {
+	$len += $1 ? length($1) * 2 : 1;
+    }
+    $len;
+}
+
+sub _sub_uniqstr {
+    my $n = 0;
+    sub {
+	my $len = shift;
+	croak "Illegal length" if $len < 2;
+	croak "Too many arguments" if $n >= 25;
+	my $s = pack("CC", $n / 5 + 1, $n % 5 + 1) . ("_" x ($len - 2));
+	$n++;
+	$s;
+    }
 }
 
 1;
