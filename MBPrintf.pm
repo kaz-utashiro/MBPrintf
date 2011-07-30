9c37768b50e14b531e9e203d07e441697cab5665
Kazumasa Utashiro <kaz@utashiro.com>
2011-07-30 11:57:59 +0900

    Wrote document.

--- MBPrintf.pm
+++ MBPrintf.pm
@@ -9,7 +9,7 @@ BEGIN {
     use Exporter   ();
     our ($VERSION, @ISA, @EXPORT, @EXPORT_OK, %EXPORT_TAGS);
 
-    $VERSION = sprintf "%d.%03d", q$Revision: 1.3 $ =~ /(\d+)/g;
+    $VERSION = sprintf "%d.%03d", q$Revision: 1.4 $ =~ /(\d+)/g;
 
     @ISA         = qw(Exporter);
     @EXPORT      = qw(&mbprintf &mbsprintf);
@@ -73,3 +73,61 @@ sub _sub_uniqstr {
 }
 
 1;
+
+__END__
+
+=head1 NAME
+
+MBPrintf - printf family functions to handle multi-byte characters
+
+=head1 SYNOPSIS
+
+use MBPrintf;
+
+mbprintf(FORMAT, LIST)
+
+mbsprintf(FORMAT, LIST)
+
+=head1 DESCRIPTION
+
+C<MBPrintf> is a almost-printf-compatible library with a capability of handling multi-byte wide characters properly.
+
+=head1 FUNCTIONS
+
+=over 4
+
+=item mbprintf(FORMAT, LIST)
+
+=item mbsprintf(FORMAT, LIST)
+
+Use just like perl's I<printf> and I<sprintf> functions
+except that I<printf> does not take FILEHANDLE as a first argument.
+
+=back
+
+=head1 IMPLEMENTATION NOTES
+
+Strings in the LIST which contains wide-width character are replaced
+before formatting, and recovered after the process.  Number of
+replaced arguments are limited by 25.
+
+Unique replacement string contains a combination of control characters
+(Control-A to Control-E).  So, if the FORMAT contains a string in this
+range, it has a chance to be a subject of replacement.
+
+Wide-character judgement is done based on Unicode property
+B<East_Asian_Width> is B<Wide> or B<FullWidth>.  There is another
+value B<Ambiguous> and treatment of this type characters are literaly
+B<ambiguous> and is not considered now.  It might be better to use
+L<Unicode::EastAsianWidth> instead.
+
+
+=head1 AUTHOR
+
+Copyright (c) 2011 Kazumasa Utashiro.  All rights reserved.
+
+=head1 LICENSE
+
+See L<http://www.perl.com/perl/misc/Artistic.html>
+
+=cut
